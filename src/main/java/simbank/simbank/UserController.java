9323457fec6127b5254cf2ee9b026112b08c1591
VHPT <vhpt@TECHIN.INTERNAL>
2025-06-05 08:30:09 +0300

    api changes 2

--- src/main/java/simbank/simbank/UserController.java
+++ src/main/java/simbank/simbank/UserController.java
@@ -4,9 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -25,12 +22,15 @@ public class UserController {
     public String getMethodName(@RequestParam String param) {
         return new String();
     }
-    
-    @GetMapping("/clients")
+   
     public List<Client> getAllClients() {
-        return userService.getAllClients();
+        return userService.getAllClients();    
     }
 
+    public List<Employee> getAllEmployees() {
+        return userService.getAllEmployees();
+    }
+    
     @GetMapping("/rand")
     public EmployeesAndClientsResponse getAll() {
         ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
@@ -39,36 +39,24 @@ public class UserController {
         if(ctrl.getSessionNoBS(session).equals("admin")){
             resp.setEmployees(getAllEmployees());
         }
-        if(!ctrl.getSessionNoBS(session).equals("404")){
-            resp.setClients(getAllClients());
-        }
+        resp.setClients(getAllClients());
         return resp;
     }
     
-
+    /*
     // Get user by id
     @GetMapping("/clients/{id}")
     public Client getClientById(@PathVariable Long id) {
         return userService.getClientById(id);
     }
   
-    @GetMapping("/employees")
-    public List<Employee> getAllEmployees() {
-        return userService.getAllEmployees();
-    }
+    
 
     // Get user by id
     @GetMapping("/employees/{id}")
     public Employee getEmployeeById(@PathVariable Long id) {
         return userService.getEmployeeById(id);
-    }
-  
-    @PostMapping("/login")
-    public Object login(@RequestBody LoginRequest loginRequest) {
-        String username = loginRequest.getUsername();
-        String password = loginRequest.getPassword();
-        return userService.login(username, password);
-    }
+    }*/
     
 }
 class LoginRequest {
